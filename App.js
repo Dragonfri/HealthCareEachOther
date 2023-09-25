2f3768c43dfe0a09f8c89bef29e2d826b73706a6
minseok cho <whalstjr199927@gmail.com>
2023-09-25 10:34:07 +0900

    회원가입 초기 세팅

---
+++ App.js
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+function App() {
+  return <Text>회원가입 부터 시작이요~</Text>;
+}
+
+export default App;
