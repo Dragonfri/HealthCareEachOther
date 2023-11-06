8a89835285dd208c236b0b65d98a336dd7649ae9
minseok cho <whalstjr199927@gmail.com>
2023-11-06 11:44:50 +0900

    d 알람 생성 브랜치 생성

---
+++ component/createAlarm/createAlarmScreen.js
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import {React, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  TouchableOpacity,
+  View,
+  Image,
+} from 'react-native';
+import SelectScreen from './selectScreen';
+import ConstructGroup from './constructGroup';
+import ParticipateGroup from './paticipateGroup';
